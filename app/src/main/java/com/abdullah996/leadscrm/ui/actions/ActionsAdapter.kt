a023402632feab265d8f211123429b0a5f18eb3f
abdullah <abdullaheladly@gmail.com>
2022-02-03 15:04:21 +0200

    add date to actions

--- app/src/main/java/com/abdullah996/leadscrm/ui/actions/ActionsAdapter.kt
+++ app/src/main/java/com/abdullah996/leadscrm/ui/actions/ActionsAdapter.kt
@@ -1,18 +1,20 @@
 package com.abdullah996.leadscrm.ui.actions
 
+import android.annotation.SuppressLint
+import android.os.Build
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.Button
 import android.widget.TextView
 import androidx.recyclerview.widget.DiffUtil
 import androidx.recyclerview.widget.RecyclerView
 import com.abdullah996.leadscrm.R
 import com.abdullah996.leadscrm.model.getstatus.Actions
-import com.abdullah996.leadscrm.model.getstatus.AllStatusReponse
-import com.abdullah996.leadscrm.model.leeds.Leads
-import com.abdullah996.leadscrm.ui.home.LeadsAdapter
 import com.abdullah996.leadscrm.utill.DiffUtilCallBack
+import java.text.SimpleDateFormat
+import java.time.LocalDate
+import java.time.format.DateTimeFormatter
+import java.util.*
 
 class ActionsAdapter():RecyclerView.Adapter<ActionsAdapter.MyViewHolder>() {
 
@@ -27,11 +29,10 @@ class ActionsAdapter():RecyclerView.Adapter<ActionsAdapter.MyViewHolder>() {
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): MyViewHolder {
         return MyViewHolder(LayoutInflater.from(parent.context).inflate(R.layout.action_card,parent,false))
     }
-
     override fun onBindViewHolder(holder: MyViewHolder, position: Int) {
         holder.itemView.findViewById<TextView>(R.id.txt_action_action).text=actionsList[position].name.toString()
         holder.itemView.findViewById<TextView>(R.id.txt_comment_action).text=actionsList[position].comment.toString()
-
+        holder.itemView.findViewById<TextView>(R.id.txt_date_action).text=actionsList[position].createdAt.take(10)
     }
 
     override fun getItemCount(): Int {
